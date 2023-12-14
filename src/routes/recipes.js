1ae689bb7afe7ce3db47e0e0ca1f75e6630f2717
Felipe Vergara <felipe.vergara19@gmail.com>
2023-12-14 17:09:48 -0300

    Crear Ruta post de recetas

---
+++ src/routes/recipes.js
@@ -0,0 +1,16 @@
+// Agregar las dependencias de la ruta
+const express = require*(express);
+const router = express.Router();
+const Recipe = require('../models/recipes');
+
+router.post('/addRecepi', async (req,res) => {
+  try {
+    const newRecepi = new Recipe(req.body);
+    await newRecepi.save();
+    res.status(201).json({mensaje: 'Receta Agregada Exitozamente', recipe: newRecepi});  
+  } catch (error) {
+    res.status(500).json({error: 'Erro al agregar la receta'});
+  }
+});
+
+module.exports = router;
