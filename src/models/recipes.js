d3fd0bfad567a6bc92100330277aa94bf9692e50
Felipe Vergara <felipe.vergara19@gmail.com>
2023-12-14 13:05:40 -0300

    Creacion esquema ingredietns

---
+++ src/models/recipes.js
@@ -0,0 +1,17 @@
+const mongoose = require ('mongoose');
+const Ingredient = require ('./ingredients')
+
+const typeRecipe = ['Breakfast', 'Launch', 'Dessert'];
+
+
+const recipeSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  description: {type: String, required: true},
+  calories: {type: Number, required: true},
+  ingredients: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Ingrediente' }],
+  type: {type: String, enum:typeRecipe, required: true},
+})
+
+const Recipe = mongoose.model('Recipe', recipeSchema)
+
+model.exports = Recipe;
