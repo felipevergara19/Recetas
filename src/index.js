30876f9c41f51dcbcec0cccd4d045ff2126b2016
Felipe Vergara <felipe.vergara19@gmail.com>
2023-12-14 11:45:57 -0300

    Modificacion a la conexion, exitosa

--- src/index.js
+++ src/index.js
@@ -8,18 +8,18 @@ const app = express();
 
 const connectToDatabase = async() => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser : true,
-      useUnifiedTopology : true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('Conexion Exitosa')
   } catch (error) {
     console.error('Error al conectar a la DB', error.message);
     process.exit(1);
   }
 }
+//llamada a la conexcion de base de datos
 
-const PORT = process.send.PORT || 3000;
+connectToDatabase();
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto${PORT}`);
